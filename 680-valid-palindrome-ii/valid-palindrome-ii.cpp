fc7237e737f646951af91c3c985a19916a071b4e
Ritikagrawal07 <103807116+Ritikagrawal07@users.noreply.github.com>
2026-02-05 16:10:45 +0530

    Time: 4 ms (59.28%) | Memory: 26.6 MB (41.81%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+bool isPalindrome(string s,int l ,int r){
+     
+        while(l< r){
+            if(s[l] != s[r]){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+}
+
+
+    bool validPalindrome(string s) {
+        int left = 0; 
+        int right = s.length()-1;
+
+        while(left < right){
+            if(s[left] != s[right]){
+                return isPalindrome(s,left+1,right) ||
+                       isPalindrome(s,left,right-1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
