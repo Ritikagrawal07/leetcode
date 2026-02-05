997b11f8c98818d3173a5ea2a8126869b5a6adf8
Ritikagrawal07 <103807116+Ritikagrawal07@users.noreply.github.com>
2026-02-05 15:51:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (54.24%) - LeetSync

--- 742-to-lower-case/to-lower-case.cpp
+++ 742-to-lower-case/to-lower-case.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-       string ans = "";
+       
        for(int i  =0  ; i < s.length() ; i++){
-        ans += tolower(s[i]);
-       }return ans;
+        s[i] = tolower(s[i]);
+       }return s;
     }
 };
