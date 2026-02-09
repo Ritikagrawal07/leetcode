9dec35c4611a08991eb754ead2ddab028e38ac91
Ritikagrawal07 <103807116+Ritikagrawal07@users.noreply.github.com>
2026-02-09 19:51:37 +0530

    Time: 2 ms (47.78%) | Memory: 9.4 MB (56.05%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        
+        unordered_map<char,char>StoTmap;
+        unordered_map<char,char>TtoSmap;
+
+        for(int i = 0 ;  i < s.length() ; i++){
+
+            if(!StoTmap[s[i]]  && !TtoSmap[t[i]]){
+                StoTmap[s[i]] = t[i];
+                TtoSmap[t[i]] = s[i];
+            }
+            else if(StoTmap[s[i]] != t[i]){
+                return false;
+            }
+            else if(TtoSmap[t[i]] != s[i]){
+                return false;
+            }
+        }return true;
+    }
+};
